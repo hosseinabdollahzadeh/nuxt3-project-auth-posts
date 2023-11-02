e32f9d23b93d9cdf2f875a65498dfa96886e6fbd
Hossein <abdollahzadeh.hossein@gmail.com>
2023-11-02 08:29:04 +0330

    Create header & install bootstrap

---
+++ components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav class="navbar navbar-expand-lg bg-light">
+    <div class="container">
+      <NuxtLink class="navbar-brand" to="/">Nuxt3 project</NuxtLink>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+              aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <NuxtLink class="nav-link active" to="/">Home</NuxtLink>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Posts</a>
+          </li>
+        </ul>
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Profile</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Logout</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Register</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Login</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
