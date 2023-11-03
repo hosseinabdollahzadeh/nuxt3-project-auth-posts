1c91d9b765d5b4e83a6a6759eec503ef6ee1b923
Hossein <abdollahzadeh.hossein@gmail.com>
2023-11-03 13:19:43 +0330

    Logout

--- components/Header.vue
+++ components/Header.vue
@@ -21,7 +21,7 @@
             <a class="nav-link" href="#">Profile</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Logout</a>
+            <a class="nav-link" @click="logout" href="#">Logout</a>
           </li>
           <li class="nav-item">
             <NuxtLink class="nav-link" activeClass="active" to="/auth/register">Register</NuxtLink>
@@ -34,3 +34,15 @@
     </div>
   </nav>
 </template>
+
+<script setup>
+import { useToast } from "vue-toastification";
+const toast = useToast()
+async function logout(){
+  await useFetch('/api/auth/logout',{
+    method: 'POST'
+  })
+  toast.warning("You are logouted!")
+  return navigateTo('/')
+}
+</script>
